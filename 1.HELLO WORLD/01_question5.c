609832b663a60c61303a8d1b002e89de1dc93182
Swapnxll <130583218+Swapnxll@users.noreply.github.com>
2023-04-13 00:42:38 +0530

    Add files via upload

---
+++ 1.HELLO WORLD/01_question5.c
@@ -0,0 +1,9 @@
+//predict what will be printed
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int a=3;
+    printf("%d %d %d\n",a,++a,a++); //printf takes values from right to left
+    return 0;
+}
