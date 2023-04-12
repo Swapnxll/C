d7ce425bb93b10bf3cf6ac72e0618e37fa3b24fb
Swapnxll <130583218+Swapnxll@users.noreply.github.com>
2023-04-12 23:13:09 +0530

    Delete HELLO WORLD directory

--- HELLO WORLD/01question4.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-
-int main() {
-    int principal=23,rate=56,time=12;
-    float simple_interest = (principal*rate*time)/100;
-    printf("the value of simple interest is %f \n", simple_interest);
-    return 0;
-}
