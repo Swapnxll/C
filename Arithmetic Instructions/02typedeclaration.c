146e6fd3be5b490974c21aff211ae59c1a40fe22
Swapnxll <130583218+Swapnxll@users.noreply.github.com>
2023-04-12 23:06:44 +0530

    Delete Arithmetic Instructions directory

--- Arithmetic Instructions/02typedeclaration.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h> 
-
-int main() {
-    int a =6,b =7, c=8; // Type delaration instruction
-    b = c = a; //reverse of last two lines will create error
-    printf("value of b is %d \n", b);
-    return 0;
-}
